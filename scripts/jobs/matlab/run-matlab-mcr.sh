983e68d446a23a697a898d0540931c4879c1d4df
Dr. Ali Kerrache <ali.kerrache@googlemail.com>
2022-10-16 11:54:47 -0500

    Update job scripts

--- scripts/jobs/matlab/run-matlab-mcr.sh
+++ scripts/jobs/matlab/run-matlab-mcr.sh
@@ -4,8 +4,9 @@
 #SBATCH --mem-per-cpu=4000M
 #SBATCH --time=0-3:00:00
 #SBATCH --job-name=Matlab-mcr-job
+#SBATCH --partition=compute
 
-# Choose the MCR directory according to the compiler version used
+# Choose the MCR directory according to the compiler version used.
 # The one below for uofm/matlab/R2017A
 
 MCR=/global/software/matlab/mcr/v93
